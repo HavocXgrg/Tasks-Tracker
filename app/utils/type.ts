7ed8d8d2647226d707539c1b48c8028df8b6e1d1
bishwa <108havocx108@gmail.com>
2025-12-28 23:29:58 +0545

    updated the types and interfaces

--- app/utils/type.ts
+++ app/utils/type.ts
@@ -1,9 +1,8 @@
 export interface Task {
     id: number;
     title: string;
-    dueDate: string; 
-    status: 'Pending' | 'Completed';
-  }
-  
-  export type FilterType = 'All' | 'Completed' | 'Pending';
-  export type SortType = 'name' | 'date' | null;
+    dueDate: string;
+    status: "Pending" | "Completed";
+}
+export type FilterType = "All" | "Completed" | "Pending";
+export type SortType = "name" | "date" | null;
